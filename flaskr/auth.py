7a77f65d8b9bfffd32fec0dbebe1cdddb51af1ad
suuu0122 <suuu0122lab@gmail.com>
2023-04-01 00:24:20 +0900

    fix miss typing

--- flaskr/auth.py
+++ flaskr/auth.py
@@ -177,7 +177,7 @@ load_logged_in_user():
 '''
 @bp.before_app_request
 def load_logged_in_user():
-    user_id = session.get('usr_id')
+    user_id = session.get('user_id')
 
     if user_id is None:
         g.user = None
